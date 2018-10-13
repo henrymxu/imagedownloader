a6f278df40adba887f2fbe0c02f8fc4e1597731d
Henry Xu <henry.m.xu@live.com>
2018-10-12 20:07:13 -0400

    Reorganize package

---
+++ _examples/main.go
@@ -0,0 +1,41 @@
+package _examples
+
+import (
+	"fmt"
+	"github.com/henrymxu/imagedownloader"
+	"github.com/henrymxu/imagedownloader/imgsrcs"
+	"github.com/henrymxu/imagedownloader/utils/program"
+	"github.com/henrymxu/imagedownloader/utils/storage"
+	"strings"
+)
+
+// params: config file path, search, folder name, number of images, tags to exclude ...
+func main() {
+	params := program.GetInitialParams()
+
+	configPath := params.ConfigPath
+	search := params.Search
+	folder := params.Folder
+	count := params.Count
+	excludesearch := params.Excludes
+
+	config := storage.LoadConfig(configPath)
+	goroutineCount := config.GoRoutineCount
+
+	// Create an ImageSource
+	var imageSource imagesources.ImageSource
+	imageSource = imagesources.New(config.FlickrApiKey)
+
+	// Configure path from parameters
+	imageNameFormat := config.ImagesNameFormat
+	if !strings.Contains(imageNameFormat, "%d") {
+		imageNameFormat = fmt.Sprintf("%s_%%d.jpg", imageNameFormat)
+	}
+	imageFolder := fmt.Sprintf("%s/%s", config.ImagesFolder, folder)
+	path := fmt.Sprintf("%s/%s/%s", storage.GetHomeDir(), imageFolder, imageNameFormat)
+
+	// Run Image Downloader
+	var imgdlr = imagedownloader.New(imageSource, goroutineCount)
+	imgdlr.DownloadImages(path, count, search, excludesearch)
+}
+
---
+++ imagedownloader.go
@@ -0,0 +1,63 @@
+package imagedownloader
+
+import (
+	"fmt"
+	"github.com/henrymxu/imagedownloader/imgsrcs"
+	"github.com/henrymxu/imagedownloader/utils/network"
+	"github.com/henrymxu/imagedownloader/utils/storage"
+	"sync"
+	"time"
+)
+
+type imagedownloader struct {
+	source imagesources.ImageSource
+	goroutineCount int
+}
+
+func New(source imagesources.ImageSource, goroutineCount int) imagedownloader {
+	o := imagedownloader{source, goroutineCount}
+	return o
+}
+
+func (imgdlr imagedownloader) DownloadImages(path string, count int, search string, excludes []string) {
+	urls := imgdlr.source.GetImageUrls(count, search, excludes...)
+	imgdlr.downloadAllImages(path, urls)
+}
+
+
+func (imgdlr imagedownloader) downloadAllImages(path string, imageurls []string) {
+	start := time.Now()
+	var imageUrlMaps = imgdlr.splitUpImageUrls(imgdlr.goroutineCount, imageurls)
+	var wg sync.WaitGroup
+	for i := 0; i < imgdlr.goroutineCount; i++ {
+		wg.Add(1)
+		index := i
+		go func() {
+			defer wg.Done()
+			for key, value := range imageUrlMaps[index] {
+				imageData := network.MakeSimpleRequest(key)
+				storage.SaveToDisc(imageData, fmt.Sprintf(path, value))
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+// This function splits up the list of image urls into smaller maps so a single goroutine can service multiple images
+// Returns a map with key image url, and value of original index
+func (imgdlr imagedownloader) splitUpImageUrls(count int, imageUrls []string) []map[string]int {
+	if imgdlr.goroutineCount > count {
+		imgdlr.goroutineCount = count
+	}
+	var imageUrlMaps = make([]map[string]int, count)
+	for i := 0; i < len(imageUrls); i += count {
+		for j := i; j < i + count; j++ {
+			if imageUrlMaps[j - i] == nil {
+				imageUrlMaps[j - i] = make(map[string]int)
+			}
+			imageUrlMaps[j - i][imageUrls[j]] = j
+		}
+	}
+	return imageUrlMaps
+}
